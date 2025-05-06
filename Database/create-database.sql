cbee3d0aa33a642ff4a92f856efebc90b8f129cb
HeraldoSouza <heraldodesousa@gmail.com>
2025-05-06 12:15:26 -0300

    Projeto fullstack com docker compose

---
+++ Database/create-database.sql
@@ -0,0 +1,9 @@
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'RommanelDB')
+BEGIN
+    CREATE DATABASE RommanelDB;
+    PRINT 'Database RommanelDB created.'
+END
+ELSE
+BEGIN
+    PRINT 'Database RommanelDB already exists.'
+END
